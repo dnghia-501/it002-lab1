fd2b97e8047fa4010d6f7d31b3154e98e6072c4e
dnghia <dnghiatr@gmail.com>
2026-03-24 16:53:49 +0700

    Add files via upload

--- lab1.5.cpp
+++ lab1.5.cpp
@@ -1,33 +1,65 @@
 #include <iostream>
 #include <string>
-#include <cmath>
-
+#include <cctype> 
 using namespace std;
-
-struct hs {
+struct HocSinh {
     string hoten;
     double toan;
     double van;
     double dtb;
 };
-void input(hs &hs)
-{
-    cout << "nhaphoten" << endl;
-    getline(cin, hs.hoten);
+bool kiemtrahoten(string ten) {
+    if (ten.empty()) return false;
+
+    bool cocach = false;
+    for (int i = 0; i < ten.length(); i++) {
+        char c = ten[i];
+
+        if (!isalpha(c) && !isspace(c)) {
+            return false;
+        }
+
+        if (isspace(c)) {
+            cocach = true;
+        }
+        else {
+            if (i == 0 || ten[i - 1] == ' ') {
+                if (!isupper(c)) return false;
+            }
+            else {
+               if (!islower(c)) return false;
+            }
+        }
+    }
+    return cocach;
+}
+
+void input(HocSinh& hs) {
+    do {
+        cout << "Nhap ho ten (VD: Nguyen Van A): ";
+        getline(cin, hs.hoten);
+
+        if (!kiemtrahoten(hs.hoten)) {
+            cout << "Loi: Ho ten chua hop le " << endl;
+        }
+    } while (!kiemtrahoten(hs.hoten));
+
+    cout << "Nhap diem Toan: ";
     cin >> hs.toan;
+
+    cout << "Nhap diem Van: ";
     cin >> hs.van;
 }
-void diemtrungbinh(hs& hs) 
-{
+void diemtrungbinh(HocSinh& hs) {
     hs.dtb = (hs.toan + hs.van) / 2.0;
 }
-int main()
-{
-    hs hs;
+
+int main() {
+    HocSinh hs; 
+
     input(hs);
     diemtrungbinh(hs);
     cout << "Hoc sinh: " << hs.hoten << " | Diem TB: " << hs.dtb << endl;
 
     return 0;
 }
-
